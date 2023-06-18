18dba04669f06eb30efcf8716d067b2f30c01891
Tumelo070 <davespain9@gmail.com>
2023-06-18 03:58:56 -0700

    t

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main-  Entry point
- * return: 1 (success)
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
 	write(2,
