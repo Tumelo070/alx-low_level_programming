def68fac01249b978576962acc8f18587ddb316b
Tumelo070 <davespain9@gmail.com>
2023-06-18 02:48:44 -0700

    hot fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <stdio.h> 
 /**
- * main- Entry point
+ * main - Entry point 
  *
- * return- 0 indicates a success
-*/
+ * return: 0 (success)
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
 
