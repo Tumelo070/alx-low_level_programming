88198d54c5ea17d685bf0154443aae01fc0f34bd
Tumelo070 <davespain9@gmail.com>
2023-06-20 03:41:46 -0700

    tumelo

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print from a to z 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int l;
+
+	for (letter = a; letter <= z; letter++)
+	{
+		for (l = 1; l <= 10; l++)
+			_putchar('letter);
+	}
+		{
+			_putchar('\n');
+			i++;
+		}
+		return (0);
+}
