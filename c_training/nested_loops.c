22f10ca8aa822a14d0c01b9daa89a78ef5debed3
Tumelo070 <davespain9@gmail.com>
2023-06-23 12:36:28 -0700

    t

---
+++ c_training/nested_loops.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- Learning basic loop program
+ *Return: 0 (success)
+ */
+
+int main(void)
+{
+	int j;
+	for (j = 1; j <= 10; j++)
+	{
+	printf("%d", j);
+	}
+	return (0);
+}
---
+++ c_training/playing.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Counting numbers from 10 to 50
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int q;
+
+	for (q = 10; q > 50; q++)
+	{
+	printf("%d\n", q);
+	}
+	return (0);
+}
